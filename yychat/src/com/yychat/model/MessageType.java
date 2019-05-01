fcc2e0259526ce949b0424a7a537aa3e408fe9c7
kadbell <1394974991@qq.com>
2019-05-01 22:43:47 +0800

    提交文件

--- yychat/src/com/yychat/model/MessageType.java
+++ yychat/src/com/yychat/model/MessageType.java
@@ -6,5 +6,6 @@ public interface MessageType {
 	String message_Common="2";
 	String message_RequestOnlineFriend="3";//客户端请求获得在线好友信息 
 	String message_OnlineFriend="4";//服务器返回在线好友信息
+	String message_NewOnlineFriend="5";
 
 }
